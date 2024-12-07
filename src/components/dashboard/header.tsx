44dd4c2a1ca34e8d8051828bcbccaf528d16b468
Usama Tariq <usama.tariq1337@gmail.com>
2024-12-07 17:50:45 +0500

    feature(ui): theme provider and theme toggle

--- src/components/dashboard/header.tsx
+++ src/components/dashboard/header.tsx
@@ -2,26 +2,36 @@ import React from 'react'
 import { SidebarTrigger } from '../ui/sidebar'
 import { Separator } from '@radix-ui/react-separator'
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from '../ui/breadcrumb'
+import { ModeToggle } from '../ThemeProvider/ThemeToggle'
 
 const Header = () => {
   return (
     <header className="flex h-16 shrink-0 items-center gap-2">
-    <div className="flex items-center gap-2 px-4">
-      <SidebarTrigger className="-ml-1" />
-      <Separator orientation="vertical" className="mr-2 h-4" />
-      <Breadcrumb>
-        <BreadcrumbList>
-          <BreadcrumbItem className="hidden md:block">
-            <BreadcrumbLink href="#">
-              Building Your Application
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator className="hidden md:block" />
-          <BreadcrumbItem>
-            <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-          </BreadcrumbItem>
-        </BreadcrumbList>
-      </Breadcrumb>
+    <div className='flex items-center between justify-between w-full gap-2 px-4'>
+
+      <div className="flex items-center ">
+        <SidebarTrigger className="-ml-1" />
+        <Separator orientation="vertical" className="mr-2 h-4" />
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem className="hidden md:block">
+              <BreadcrumbLink href="#">
+                Building Your Application
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator className="hidden md:block" />
+            <BreadcrumbItem>
+              <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+
+
+      </div>
+
+      <ModeToggle />
+
+
     </div>
   </header>
   )
