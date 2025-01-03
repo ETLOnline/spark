81196396a45a896426ded5ba1b76724824a98cb7
hanzla-khalid1 <hunzlakhalid12@gmail.com>
2025-01-03 19:04:35 +0500

    issue-51 fix(authentication):  responsiveness

--- src/app/(authentication)/layout.tsx
+++ src/app/(authentication)/layout.tsx
@@ -1,16 +1,17 @@
+import { ScrollArea } from '@/src/components/ui/scroll-area'
 import React, { ReactNode } from 'react'
 
 const Authticationlayout = ({ children }: { children: ReactNode }) => {
 	return (
-		<div className='flex flex-row h-screen w-full '>
-			<div className='w-full relative md:w-1/2 '>
-				<img className='h-full w-full object-cover' src="/images/home/image.jpg" alt="signup-image" />
+		<div className='flex flex-row md:items-center w-full'>
+			<div className='w-full fixed md:relative md:w-1/2 '>
+				<img className='w-full object-cover h-screen md:h-[641px]' src="/images/home/image.jpg" alt="signup-image" />
 			</div>
-			<div className='absolute w-full h-full grid grid-cols-12  items-center md:w-1/2 md:relative '>
-				<div className='col-start-3 col-span-8 md:col-start-2 md:col-span-10 '>
-					<div className=' md:flex md:flex-col items-center justify-center'>
-						{children}
-					</div>
+			<div className='absolute w-full h-full flex justify-around md:grid grid-cols-12  md:w-1/2 md:relative 	'>
+				<div className='col-start-2 col-span-8 md:col-start-2 md:col-span-10 flex justify-center	items-center' >
+
+					{children}
+
 				</div>
 			</div>
 		</div>
