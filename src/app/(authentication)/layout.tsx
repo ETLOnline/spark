7c984e07405de56633874b809c19a8112ff8a577
hanzla-khalid1 <hunzlakhalid12@gmail.com>
2025-01-07 17:06:41 +0500

    issue-51 fix(authentication): responsiveness

--- src/app/(authentication)/layout.tsx
+++ src/app/(authentication)/layout.tsx
@@ -5,13 +5,11 @@ const Authticationlayout = ({ children }: { children: ReactNode }) => {
 	return (
 		<div className='flex flex-row md:items-center w-full'>
 			<div className='w-full fixed md:relative md:w-1/2 '>
-				<img className='w-full object-cover h-screen md:h-[641px]' src="/images/home/image.jpg" alt="signup-image" />
+				<img className='w-full object-cover h-screen md:h-full md:fixed md:top-0 md:w-1/2' src="/images/home/image.jpg" alt="signup-image" />
 			</div>
 			<div className='absolute w-full h-full flex justify-around md:grid grid-cols-12  md:w-1/2 md:relative 	'>
-				<div className='col-start-2 col-span-8 md:col-start-2 md:col-span-10 flex justify-center	items-center' >
-
+				<div className='col-start-2 col-span-8 md:col-start-2 md:col-span-10 flex justify-center items-center h-full' >
 					{children}
-
 				</div>
 			</div>
 		</div>
