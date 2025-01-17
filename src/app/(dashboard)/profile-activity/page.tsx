63b65816d97283a2f7e091224aefb6206642bbb4
Ammar Waheed <blazingbrawler@gmail.com>
2025-01-17 11:41:35 +0500

    activity data fetched

--- src/app/(dashboard)/profile-activity/page.tsx
+++ src/app/(dashboard)/profile-activity/page.tsx
@@ -1,5 +1,8 @@
 import ActivityScreen from "@/src/components/Dashboard/Activity/ActivityScreen"
-import { ProfileActivity } from "@/src/components/Dashboard/Activity/types/activity.types"
+import {
+  ActivityType,
+  ProfileActivity
+} from "@/src/components/Dashboard/Activity/types/activity.types.d"
 import {
   Card,
   CardContent,
@@ -10,10 +13,42 @@ import {
 import { Tabs, TabsList, TabsTrigger } from "@/src/components/ui/tabs"
 import { GetContactsAction } from "@/src/server-actions/Contact/Contact"
 import { AuthUserAction } from "@/src/server-actions/User/AuthUserAction"
+import { act } from "react"
 
 const ProfileActivityPage = async () => {
   const user = await AuthUserAction()
-  let activities: ProfileActivity[]=[]
+  let activities: ProfileActivity[] = []
+  if (user) {
+    try {
+      const res = await GetContactsAction(user.unique_id)
+      if (res.success) {
+        activities = res.data.map((contact) => {
+          const type =
+            (contact.user_contacts.is_accepted &&
+              ActivityType.Connect_Accepted) ||
+            (contact.user_contacts.is_requested &&
+            contact.user_contacts.contact_id === user.unique_id
+              ? ActivityType.Connect_Received
+              : ActivityType.Connect_Sent) ||
+            (contact.user_contacts.is_following && ActivityType.Following) ||
+            (contact.user_contacts.is_followed_by && ActivityType.Followed)
+          return {
+            user_id: contact.user_contacts.user_id,
+            contact_id: contact.user_contacts.contact_id,
+            name:
+              contact.users?.first_name + " " + contact.users?.last_name || "",
+            avatar: contact.users?.profile_url || "",
+            timestamp: contact.user_contacts.created_at || "",
+            type
+          }
+        })
+      } else {
+        throw res.error
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
   return (
     <Card className="w-full max-w-4xl mx-auto">
--- src/components/NotificationItem/NotifictionItem.tsx
+++ src/components/NotificationItem/NotifictionItem.tsx
@@ -1,5 +1,8 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar"
-import { ProfileActivity } from "@/src/app/(dashboard)/profile-activity/page"
+import {
+  ActivityType,
+  ProfileActivity
+} from "../Dashboard/Activity/types/activity.types.d"
 
 type NotificationItemProps = {
   activity: ProfileActivity
@@ -13,17 +16,23 @@ const NotificationItem: React.FC<NotificationItemProps> = ({
   return (
     <div className="flex items-center space-x-4 p-4 border-b last:border-b-0">
       <Avatar className="h-12 w-12">
-        <AvatarImage src={activity.avatar} alt={activity.name} />
+        <AvatarImage
+          className="rounded-full"
+          src={activity.avatar}
+          alt={activity.name}
+        />
         <AvatarFallback>{activity.name[0]}</AvatarFallback>
       </Avatar>
       <div className="flex-1 min-w-0">
         <p className="text-sm font-medium truncate">{activity.name}</p>
         <p className="text-sm text-muted-foreground">
-          {activity.action === "request" && "Sent you a connection request"}
-          {activity.action === "visit" && "Viewed your profile"}
-          {activity.action === "sent" && "You sent a connection request"}
-          {activity.action === "following" && "You are following"}
-          {activity.action === "follower" && "Is following you"}
+          {activity.type === ActivityType.Connect_Received &&
+            "Sent you a connection request"}
+          {activity.type === ActivityType.Visited && "Viewed your profile"}
+          {activity.type === ActivityType.Connect_Sent &&
+            "You sent a connection request"}
+          {activity.type === ActivityType.Following && "You are following"}
+          {activity.type === ActivityType.Followed && "Is following you"}
         </p>
         <p className="text-xs text-muted-foreground">
           {new Date(activity.timestamp).toLocaleString()}
