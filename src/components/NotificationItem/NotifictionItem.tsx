ae36ff385b2c100d31a3f91e300d62b7fa4686ce
Ammar Waheed <blazingbrawler@gmail.com>
2025-01-31 12:47:39 +0500

    profile links

--- src/components/NotificationItem/NotifictionItem.tsx
+++ src/components/NotificationItem/NotifictionItem.tsx
@@ -2,6 +2,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar"
 import { useAtomValue } from "jotai"
 import { userStore } from "@/src/store/user/userStore"
 import { ProfileActivity } from "../Dashboard/ProfileActivity/types/activity.types"
+import Link from "next/link"
 
 type NotificationItemProps = {
   activity: ProfileActivity
@@ -41,15 +42,20 @@ const NotificationItem: React.FC<NotificationItemProps> = ({
     <div className="flex items-center justify-between p-4 border-b last:border-b-0 max-[622px]:flex-col max-[622px]:items-start max-[622px]:space-x-0 max-[622px]:space-y-4">
       <div className="flex items-center space-x-4">
         <Avatar className="h-12 w-12">
-          <AvatarImage
-            className="rounded-full"
-            src={activity.otherUser.profile_url as string}
-            alt={name}
-          />
-          <AvatarFallback>{activity.otherUser.first_name}</AvatarFallback>
+          <Link href={`/profile/${activity.otherUser.unique_id}`}>
+            <AvatarImage
+              className="rounded-full"
+              src={activity.otherUser.profile_url as string}
+              alt={name}
+            />
+            <AvatarFallback>{activity.otherUser.first_name}</AvatarFallback>
+          </Link>
         </Avatar>
         <div className="flex-1 min-w-0">
-          <p className="text-sm font-medium truncate">{name}</p>
+          <Link href={`/profile/${activity.otherUser.unique_id}`}>
+            {" "}
+            <p className="text-sm font-medium truncate">{name}</p>
+          </Link>
           <p className="text-sm text-muted-foreground">
             {generateNotificationText(activity)}
           </p>
