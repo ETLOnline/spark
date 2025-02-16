9ff20ec055643c74b6b0e51fa6b3745173bce26a
Ammar Waheed <blazingbrawler@gmail.com>
2025-02-16 21:50:33 +0500

    commenting

--- src/components/Dashboard/posts/post-comment-form.tsx
+++ src/components/Dashboard/posts/post-comment-form.tsx
@@ -1,39 +1,52 @@
-"use client"
-
 import { Button } from "@/src/components/ui/button"
 import { useRef } from "react"
 import { Input } from "@/src/components/ui/input"
 import { Avatar, AvatarFallback, AvatarImage } from "@/src/components/ui/avatar"
 import { postStore } from "@/src/store/post/postStore"
-import { useAtom } from "jotai"
+import { useAtomValue, useSetAtom } from "jotai"
+import { useServerAction } from "@/src/hooks/useServerAction"
+import { createCommentAction } from "@/src/server-actions/Post/Post"
+import { userStore } from "@/src/store/user/userStore"
 
-type Props = {
+type PostCommentFormProps = {
   postId: number
 }
 
-const PostCommentForm: React.FC<Props> = ({ postId }) => {
+const PostCommentForm: React.FC<PostCommentFormProps> = ({ postId }) => {
   const commentText = useRef<string>("")
 
-  const [posts, setPosts] = useAtom(postStore.posts)
+  const setPosts = useSetAtom(postStore.posts)
+  const user = useAtomValue(userStore.AuthUser)
 
-  const handleAddComment = (postId: number) => {
-    const updatedPosts = posts.map((post) => {
-      if (post.id === postId) {
-        return {
-          ...post,
-          comments: [...post.comments]
-        }
-      }
-      return post
-    })
+  const [
+    createCommentLoading,
+    createdComment,
+    createCommentError,
+    createComment
+  ] = useServerAction(createCommentAction)
+
+  const name = `${user?.first_name} ${user?.last_name}`
+
+  const handleAddComment = async (postId: number) => {
+    const response = await createComment(postId, commentText.current)
+    const addedComment = response?.success ? response.data : null
+    if (addedComment) {
+      setPosts((posts) =>
+        posts.map((post) =>
+          post.id === postId
+            ? { ...post, postComments: [...post.postComments, addedComment] }
+            : post
+        )
+      )
+    }
     commentText.current = ""
   }
 
   return (
     <div className="flex items-center w-full space-x-2">
       <Avatar className="h-8 w-8 mt-4">
-        <AvatarImage src="/avatars/04.png" alt="Current User" />
-        <AvatarFallback>CU</AvatarFallback>
+        <AvatarImage src={user?.profile_url as string} alt="Current User" />
+        <AvatarFallback>{name}</AvatarFallback>
       </Avatar>
       <Input
         placeholder="Add a comment..."
