96bf3ed9874e73709013435c93c3763dd49bf1c9
Ammar Waheed <blazingbrawler@gmail.com>
2025-03-04 11:43:30 +0500

    rectification of turbopack errors

--- src/server-actions/User/User.ts
+++ src/server-actions/User/User.ts
@@ -7,7 +7,8 @@ import {
 import { CreateServerAction } from ".."
 import { AddTag } from "@/src/db/data-access/tag/query"
 import { AddUserTag, DeleteUserTags } from "@/src/db/data-access/tag/query"
-import { Profile, ProfileData } from "@/src/components/Dashboard/profile/types/profile-types"
+import { ProfileData } from "@/src/components/Dashboard/profile/types/profile-types"
+import { Tag, TagStatus } from "@/src/components/TagsInput/tags-input-types"
 
 export const UpdateBioForUserAction = CreateServerAction(
   true,
@@ -30,6 +31,8 @@ export const SaveUserProfileAction = CreateServerAction(
   true,
   async (profileData: ProfileData) => {
     try {
+      let updatedSkillTags: Tag[] = []
+      let updatedInterestTags: Tag[] = []
       await UpdateUserBio(profileData.userId, profileData.bio)
       // add new tags
       if (profileData.newTags.length) {
@@ -39,6 +42,19 @@ export const SaveUserProfileAction = CreateServerAction(
             return { user_id: profileData.userId, tag_id: tag.id }
           })
         )
+        insertedTags.forEach((tag) => {
+          tag.type === "skill"
+            ? updatedSkillTags.push({
+                id: tag.id,
+                name: tag.name,
+                status: TagStatus.saved
+              })
+            : updatedInterestTags.push({
+                id: tag.id,
+                name: tag.name,
+                status: TagStatus.saved
+              })
+        })
       }
       // add existing tags
       if (profileData.existingTags.length) {
@@ -47,17 +63,31 @@ export const SaveUserProfileAction = CreateServerAction(
             return { user_id: profileData.userId, tag_id: tag.id as number }
           })
         )
+        profileData.existingTags.forEach((tag) => {
+          tag.type === "skill"
+            ? updatedSkillTags.push({
+                id: tag.id,
+                name: tag.name,
+                status: TagStatus.saved
+              })
+            : updatedInterestTags.push({
+                id: tag.id,
+                name: tag.name,
+                status: TagStatus.saved
+              })
+        })
       }
-
       // delete tags
       if (profileData.deletedTagsIds.length) {
         await DeleteUserTags(profileData.userId, profileData.deletedTagsIds)
       }
 
       return {
-        success: true
+        success: true,
+        data: { skills: updatedSkillTags, interests: updatedInterestTags }
       }
     } catch (error) {
+      console.error(error)
       return {
         success: false,
         error: error
@@ -80,7 +110,7 @@ export const GetUserProfileAction = CreateServerAction(
         }
       )
       return {
-        data: {...profileData, recommendations: mappedRecommendations},
+        data: { ...profileData, recommendations: mappedRecommendations },
         success: true
       }
     } catch (error) {
