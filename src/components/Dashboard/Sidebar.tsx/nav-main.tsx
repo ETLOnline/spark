b862d850ec553f0ce3d8b367018db698b00bf78b
Ammar Waheed <33984786+BlazingC0der@users.noreply.github.com>
2025-04-07 19:27:59 +0500

    sidebar truncations

--- src/components/Dashboard/Sidebar.tsx/nav-main.tsx
+++ src/components/Dashboard/Sidebar.tsx/nav-main.tsx
@@ -19,6 +19,8 @@ import {
 } from "@/src/components/ui/sidebar"
 import Link from "next/link"
 import { NavItem } from "./nav-types"
+import { Tooltip, TooltipContent, TooltipTrigger } from "../../ui/tooltip"
+import { TooltipProvider } from "@radix-ui/react-tooltip"
 
 export default function NavMain({
   label,
@@ -33,46 +35,68 @@ export default function NavMain({
       <SidebarMenu>
         {items && items.length
           ? items.map((item) => (
-            <Collapsible
-              key={item.url + item.title}
-              asChild
-              defaultOpen={item.isActive}
-            >
-              <SidebarMenuItem>
-                <SidebarMenuButton asChild tooltip={item.title}>
-                  <Link href={item.url}>
-                    <item.icon />
-                    <span>{item.title}</span>
-                    {item?.isPrivate ? <Lock className="text-sm" height={10} /> : null}
-                  </Link>
-                </SidebarMenuButton>
-                {item.items?.length ? (
-                  <>
-                    <CollapsibleTrigger asChild>
-                      <SidebarMenuAction className="data-[state=open]:rotate-90">
-                        <ChevronRight />
-                        <span className="sr-only">Toggle</span>
-                      </SidebarMenuAction>
-                    </CollapsibleTrigger>
-                    <CollapsibleContent>
-                      <SidebarMenuSub>
-                        {item.items?.map((subItem) => (
-                          <SidebarMenuSubItem key={subItem.title}>
-                            <SidebarMenuSubButton asChild>
-                              <Link href={subItem.url}>
-                                {subItem.icon ? <subItem.icon /> : null}
-                                <span>{subItem.title}</span>
-                              </Link>
-                            </SidebarMenuSubButton>
-                          </SidebarMenuSubItem>
-                        ))}
-                      </SidebarMenuSub>
-                    </CollapsibleContent>
-                  </>
-                ) : null}
-              </SidebarMenuItem>
-            </Collapsible>
-          ))
+              <Collapsible
+                key={item.url + item.title}
+                asChild
+                defaultOpen={item.isActive}
+              >
+                <SidebarMenuItem>
+                  <TooltipProvider>
+                    <Tooltip delayDuration={0}>
+                      <TooltipTrigger asChild>
+                        <SidebarMenuButton asChild>
+                          <Link href={item.url}>
+                            <item.icon />
+                            <span className="truncate max-w-[200px] block">
+                              {item.title}
+                            </span>
+                            {item?.isPrivate ? (
+                              <Lock className="text-sm" height={10} />
+                            ) : null}
+                          </Link>
+                        </SidebarMenuButton>
+                      </TooltipTrigger>
+                      <TooltipContent align="start">{item.title}</TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                  {item.items?.length ? (
+                    <>
+                      <CollapsibleTrigger asChild>
+                        <SidebarMenuAction className="data-[state=open]:rotate-90">
+                          <ChevronRight />
+                          <span className="sr-only">Toggle</span>
+                        </SidebarMenuAction>
+                      </CollapsibleTrigger>
+                      <CollapsibleContent>
+                        <SidebarMenuSub>
+                          {item.items?.map((subItem) => (
+                            <SidebarMenuSubItem key={subItem.title}>
+                              <SidebarMenuSubButton asChild>
+                                <TooltipProvider>
+                                  <Tooltip delayDuration={0}>
+                                    <TooltipTrigger asChild>
+                                      <Link href={subItem.url} className="flex items-center gap-x-2">
+                                        {subItem.icon ? <subItem.icon /> : null}
+                                        <span className="truncate max-w-[200px] block">
+                                          {subItem.title}
+                                        </span>
+                                      </Link>
+                                    </TooltipTrigger>
+                                    <TooltipContent align="start">
+                                      {subItem.title}
+                                    </TooltipContent>
+                                  </Tooltip>
+                                </TooltipProvider>
+                              </SidebarMenuSubButton>
+                            </SidebarMenuSubItem>
+                          ))}
+                        </SidebarMenuSub>
+                      </CollapsibleContent>
+                    </>
+                  ) : null}
+                </SidebarMenuItem>
+              </Collapsible>
+            ))
           : null}
       </SidebarMenu>
     </SidebarGroup>
