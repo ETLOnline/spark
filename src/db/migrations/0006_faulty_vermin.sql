756bf5efa684c1726d6ae190c290f636347d010b
Arslan Shahid <arsalanshahid@muxtech.com.pk>
2025-04-10 19:52:51 +0500

    project management module complete

---
+++ src/db/migrations/0006_faulty_vermin.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `project` ADD `project_startDate` text NOT NULL;--> statement-breakpoint
+ALTER TABLE `project` ADD `project_targetDate` text NOT NULL;
--- src/server-actions/ProjectManagement/projectManagement.ts
+++ src/server-actions/ProjectManagement/projectManagement.ts
@@ -3,7 +3,7 @@
 
 import { InsertProject } from "@/src/db/schema";
 import { CreateServerAction } from "..";
-import { CreateProject, getProjects } from "@/src/db/data-access/project-management/query";
+import { CreateProject, getProjectById, getProjects } from "@/src/db/data-access/project-management/query";
 
 export const CreateProjectAction = CreateServerAction(true, async (project_data:InsertProject) => {
     try{
@@ -21,6 +21,16 @@ export const GetProjectsAction = CreateServerAction(true, async ()=>{
     try {
         const projects = await getProjects()
         return {suceess: true, data: projects}
+    } catch (error) {
+        return {error: error}
+    }
+})
+
+
+export const GetProjectByIdAction = CreateServerAction(true, async (projectId: string) => {
+    try {
+        const project = await getProjectById(projectId)
+        return {success: true, data: project}
     } catch (error) {
         return {error: error}
     }
