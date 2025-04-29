c120dcdf96f8dcfbaea13df10370405495be6d4b
Usama Tariq <usama.tariq1337@gmail.com>
2025-04-29 14:55:13 +0500

    migration fix

--- src/db/migrations/0007_cheerful_sharon_carter.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `space_chats` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`space_id` text NOT NULL,
-	`chat_id` integer NOT NULL,
-	`updated_at` text,
-	`created_at` text DEFAULT CURRENT_TIMESTAMP,
-	`deleted_at` text
-);
