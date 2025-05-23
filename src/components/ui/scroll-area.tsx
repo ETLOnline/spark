71de250ef368180902e82c28ffbf264ef54da95b
Hanzla Khalid <hunzlakhalid12@gmail.com>
2025-05-23 16:05:01 +0500

    issue-224

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -7,10 +7,8 @@ import { cn } from "@/src/lib/utils"
 
 const ScrollArea = React.forwardRef<
   React.ElementRef<typeof ScrollAreaPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root> & {
-    hideScrollbar?: boolean // Add this prop
-  }
->(({ className, children, hideScrollbar = false, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root>
+>(({ className, children, ...props }, ref) => (
   <ScrollAreaPrimitive.Root
     ref={ref}
     className={cn("relative overflow-hidden", className)}
@@ -19,7 +17,7 @@ const ScrollArea = React.forwardRef<
     <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit]">
       {children}
     </ScrollAreaPrimitive.Viewport>
-    {!hideScrollbar && <ScrollBar />}
+    <ScrollBar />
     <ScrollAreaPrimitive.Corner />
   </ScrollAreaPrimitive.Root>
 ))
