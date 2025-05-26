7e4ad441a8f8132c0b80ed3cf9302c46a7109952
Usamavirk444 <virkusama3@gmail.com>
2025-05-26 19:13:21 +0500

    testing the husk

--- src/components/Dashboard/Projects/ProjectWelcomeCard.tsx
+++ src/components/Dashboard/Projects/ProjectWelcomeCard.tsx
@@ -15,11 +15,11 @@ function WelcomeCard() {
         <div className="flex flex-wrap gap-2 justify-between items-center">
           <div className="space-y-1">
             <h3 className="text-2xl font-semibold tracking-tight">
-              Welcome to the Project Spaces
+              {/* Welcome to the Project Spaces */}
             </h3>
             <p className="text-sm text-muted-foreground">
               Here, ideas become reality. Submit, refine, and collaborate on
-              innovative project ideas or contribute to existing ones.
+              innovative project ideas or contribute to existing ones.                                   
             </p>
           </div>
           <CreateNewProject />
