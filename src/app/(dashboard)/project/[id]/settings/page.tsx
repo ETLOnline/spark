835296078a380d22234cca4388ece1def046ff2c
Usamavirk444 <virkusama3@gmail.com>
2025-06-03 20:08:59 +0500

    fix issue for project setting

--- src/app/(dashboard)/project/[id]/settings/page.tsx
+++ src/app/(dashboard)/project/[id]/settings/page.tsx
@@ -1,8 +1,21 @@
 import { ProjectSettings } from "@/src/components/Dashboard/ProjectManagement/ProjectSettings/ProjectSettings"
 import React from "react"
+import { GetProjectByIdAction } from "@/src/server-actions/ProjectManagement/projectManagement"
+import NotFound from "@/src/components/Dashboard/NotFound/NotFound"
 
-function page() {
-  return <ProjectSettings />
+interface Props {
+  params: Promise<{ id: string }>
+}
+
+async function page({ params }: Props) {
+  const { id } = await params
+  const projectId = id
+
+  const currProject = await GetProjectByIdAction(projectId)
+  if (!currProject.success || !currProject.data) {
+    return <NotFound />
+  }
+  return <ProjectSettings currProject={currProject.data} />
 }
 
 export default page
