831b6bbf85a348e021ba37003f96a65b849af26f
abdulrehmanghub <pakkhan9009@gmail.com>
2025-06-20 17:30:41 +0500

    SPRK-22: Loader added

--- src/components/Dashboard/profile/ProfileScreen.tsx
+++ src/components/Dashboard/profile/ProfileScreen.tsx
@@ -39,6 +39,8 @@ import { generateUrl, getPagePath } from "@/src/utils/helpers"
 import { useRef, useState } from "react"
 import { UpdateUserProfilePictureAction } from "@/src/server-actions/User/User"
 import { useServerAction } from "@/src/hooks/useServerAction"
+import Loader from "../../common/Loader/Loader"
+import { LoaderSizes } from "../../common/types/loader-types"
 
 type ProfileScreenProps = {
   tab?: string
@@ -84,35 +86,7 @@ export default function ProfileScreen({
     const reader = new FileReader()
     reader.onloadend = () => {
       const base64 = reader.result as string
-      UpdateUserProfilePictureAction(file.name, base64, file.type)
-        .then((res) => {
-          if (res?.success) {
-            toast({
-              title: "Profile picture updated!",
-              description:
-                "Your profile picture has been successfully updated.",
-              duration: 3000
-            })
-            window.location.reload()
-          } else {
-            toast({
-              variant: "destructive",
-              title: "Error",
-              description: res?.error || "Failed to update profile picture",
-              duration: 3000
-            })
-          }
-          setUploading(false)
-        })
-        .catch(() => {
-          toast({
-            variant: "destructive",
-            title: "Error",
-            description: "Something went wrong",
-            duration: 3000
-          })
-          setUploading(false)
-        })
+      setUploading(true)
       updateUserProfile(file.name, base64, file.type)
         .then((res) => {
           if (res?.success) {
@@ -131,6 +105,7 @@ export default function ProfileScreen({
               duration: 3000
             })
           }
+          setUploading(false)
         })
         .catch(() => {
           toast({
@@ -139,6 +114,7 @@ export default function ProfileScreen({
             description: "Something went wrong",
             duration: 3000
           })
+          setUploading(false)
         })
     }
     reader.readAsDataURL(file)
@@ -146,104 +122,119 @@ export default function ProfileScreen({
 
   return (
     <div className="container mx-auto p-6 relative">
-      <div className="mb-6 flex items-center space-x-4">
-        <div className="relative">
-          <Avatar className="h-20 w-20">
-            <AvatarImage
-              src={user?.profile_url as string}
-              alt="Profile picture"
-            />
-            <AvatarFallback>Profile</AvatarFallback>
-          </Avatar>
-          <button
-            className="absolute bottom-0 right-0 bg-background border rounded-full p-1 hover:bg-muted"
-            onClick={() => fileInputRef.current?.click()}
-            disabled={loading}
+      {loading ? (
+        <div className="flex justify-center h-screen w-full">
+          <Loader size={LoaderSizes.xl} />
+        </div>
+      ) : (
+        <>
+          <div className="mb-6 flex items-center space-x-4">
+            <div className="relative">
+              <Avatar className="h-20 w-20">
+                <AvatarImage
+                  src={user?.profile_url as string}
+                  alt="Profile picture"
+                />
+                <AvatarFallback>Profile</AvatarFallback>
+              </Avatar>
+
+              <button
+                className="absolute bottom-0 right-0 bg-background border rounded-full p-1 hover:bg-muted"
+                onClick={() => fileInputRef.current?.click()}
+                disabled={loading}
+              >
+                <PencilIcon className="h-4 w-4" />
+              </button>
+              <input
+                type="file"
+                ref={fileInputRef}
+                accept="image/*"
+                className="hidden"
+                onChange={handleFileChange}
+              />
+            </div>
+            <div>
+              <h1 className="text-2xl font-bold">
+                {user?.first_name}
+                <span>
+                  <TooltipProvider>
+                    <Tooltip>
+                      <TooltipTrigger asChild>
+                        <Button
+                          variant="outline"
+                          size="icon"
+                          className="ml-2"
+                          onClick={handleCopyUrl}
+                        >
+                          <LinkIcon className="h-2 w-2" />
+                        </Button>
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <span>Copy profile URL</span>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </span>
+              </h1>
+              <span className="text-muted-foreground">{user?.email}</span>
+            </div>
+          </div>
+          <Tabs
+            defaultValue="basic"
+            className="space-y-4"
+            value={tab || "basic"}
           >
-            <PencilIcon className="h-4 w-4" />
-          </button>
-          <input
-            type="file"
-            ref={fileInputRef}
-            accept="image/*"
-            className="hidden"
-            onChange={handleFileChange}
-          />
-        </div>
-        <div>
-          <h1 className="text-2xl font-bold">
-            {user?.first_name}
-            <span>
-              <TooltipProvider>
-                <Tooltip>
-                  <TooltipTrigger asChild>
-                    <Button
-                      variant="outline"
-                      size="icon"
-                      className="ml-2"
-                      onClick={handleCopyUrl}
-                    >
-                      <LinkIcon className="h-2 w-2" />
-                    </Button>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <span>Copy profile URL</span>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-            </span>
-          </h1>
-          <span className="text-muted-foreground">{user?.email}</span>
-        </div>
-      </div>
-      <Tabs defaultValue="basic" className="space-y-4" value={tab || "basic"}>
-        <TabsList>
-          <Link href={`?tab=basic`}>
-            <TabsTrigger value="basic">
-              <UserIcon className="mr-2 h-4 w-4" />
-              Bio/Basic
-            </TabsTrigger>
-          </Link>
-          <Link href={`?tab=rewards`}>
-            <TabsTrigger value="rewards">
-              <TrophyIcon className="mr-2 h-4 w-4" />
-              Rewards
-            </TabsTrigger>
-          </Link>
-          <Link href={`?tab=activity`}>
-            <TabsTrigger value="activity">
-              <StarIcon className="mr-2 h-4 w-4" />
-              Activity
-            </TabsTrigger>
-          </Link>
-          {/* <Link href={`?tab=calendar`}>
+            <TabsList>
+              <Link href={`?tab=basic`}>
+                <TabsTrigger value="basic">
+                  <UserIcon className="mr-2 h-4 w-4" />
+                  Bio/Basic
+                </TabsTrigger>
+              </Link>
+              <Link href={`?tab=rewards`}>
+                <TabsTrigger value="rewards">
+                  <TrophyIcon className="mr-2 h-4 w-4" />
+                  Rewards
+                </TabsTrigger>
+              </Link>
+              <Link href={`?tab=activity`}>
+                <TabsTrigger value="activity">
+                  <StarIcon className="mr-2 h-4 w-4" />
+                  Activity
+                </TabsTrigger>
+              </Link>
+              {/* <Link href={`?tab=calendar`}>
               <TabsTrigger value="calendar">
                 <CalendarIcon className="mr-2 h-4 w-4" />
                 Calendar
               </TabsTrigger>
             </Link> */}
-        </TabsList>
-        <TabsContent value="basic">
-          <ProfileBio
-            userBio={user?.bio as string}
-            recommendations={
-              profileData?.recommendations as unknown as ExtendedRecommendations[]
-            }
-            tags={profileData?.tags as SelectTag[]}
-          />
-        </TabsContent>
-        <TabsContent value="rewards">
-          <ProfileRewards rewards={profileData?.rewards as SelectReward[]} />
-        </TabsContent>
-        <TabsContent value="activity">
-          <ProfileActivities
-            activities={profileData?.activities as SelectActivity[]}
-          />
-        </TabsContent>
-        {/* <TabsContent value="calendar">
+            </TabsList>
+            <TabsContent value="basic">
+              <ProfileBio
+                userBio={user?.bio as string}
+                recommendations={
+                  profileData?.recommendations as unknown as ExtendedRecommendations[]
+                }
+                tags={profileData?.tags as SelectTag[]}
+              />
+            </TabsContent>
+            <TabsContent value="rewards">
+              <ProfileRewards
+                rewards={profileData?.rewards as SelectReward[]}
+              />
+            </TabsContent>
+            <TabsContent value="activity">
+              <ProfileActivities
+                activities={profileData?.activities as SelectActivity[]}
+              />
+            </TabsContent>
+            {/* <TabsContent value="calendar">
             <ProfileCalendar />
           </TabsContent> */}
-      </Tabs>
+          </Tabs>
+        </>
+      )}
     </div>
   )
 }
