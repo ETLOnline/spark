bf6046cfae697141cbb01b4e0c54b8ec890f2d12
Hanzla Khalid <hunzlakhalid12@gmail.com>
2025-07-03 19:02:25 +0500

    SPRK-77

--- src/components/Dashboard/ProjectManagement/SprintManagement/SprintTasks.tsx
+++ src/components/Dashboard/ProjectManagement/SprintManagement/SprintTasks.tsx
@@ -204,7 +204,7 @@ function SprintTasks({
           <AlertDialogHeader>
             <AlertDialogTitle>Are you sure?</AlertDialogTitle>
             <AlertDialogDescription>
-              This action will remove Task from the current sprint.
+              This action will move Task from the current sprint to backlog.
             </AlertDialogDescription>
           </AlertDialogHeader>
           <AlertDialogFooter>
@@ -215,7 +215,7 @@ function SprintTasks({
               }}
               loading={removeTaskLoading}
             >
-              Remove
+              Move
             </AlertDialogAction>
           </AlertDialogFooter>
         </AlertDialogContent>
