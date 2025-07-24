d1536be8a4647d8e040949115d6733613129f800
Obaid Ullah <obaidsafi31@gmail.com>
2025-07-24 18:57:07 +0500

    BUG: USER-----> USERS

--- src/components/communities/CommunityCard.tsx
+++ src/components/communities/CommunityCard.tsx
@@ -140,7 +140,7 @@ export default function CommunityCard({
                       <DropdownMenuItem asChild>
                         <Link href={`/communities/${community.slug}/users`}>
                           <User className="mr-2 h-4 w-4" />
-                          User
+                          Users
                         </Link>
                       </DropdownMenuItem>
                       {canView && (
