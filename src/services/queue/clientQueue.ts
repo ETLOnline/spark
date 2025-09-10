941543cc30b4a653f55c24a6b9423199c17e4e1c
Usamavirk444 <virkusama3@gmail.com>
2025-09-10 22:21:34 +0500

    add the azure queue and variables

---
+++ src/services/queue/clientQueue.ts
@@ -0,0 +1,20 @@
+import { QueueClient } from "@azure/storage-queue"
+
+const connectionString = process.env.AZURE_STORAGE_QUEUE_CONNECTION_STRING
+const queueName = process.env.AZURE_NOTIFICATION_QUEUE_NAME
+
+if (!connectionString) {
+  throw new Error(
+    "AZURE_STORAGE_QUEUE_CONNECTION_STRING is not set in environment variables."
+  )
+}
+
+if (!queueName) {
+  throw new Error(
+    "AZURE_NOTIFICATION_QUEUE_NAME is not set in environment variables."
+  )
+}
+
+export const queueClient = new QueueClient(connectionString!, queueName!)
+
+console.log(`Azure Queue client for '${queueName}' has been initialized.`)
