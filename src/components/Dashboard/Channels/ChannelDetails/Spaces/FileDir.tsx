3f6075d3d8ab81841516d86aa1f2aec43035cf38
Mazhar Manzoor <112794655+Mazharcodemaster@users.noreply.github.com>
2025-11-11 14:33:49 +0500

    SW-17-truncate and fixed folder name

--- src/components/Dashboard/Channels/ChannelDetails/Spaces/FileDir.tsx
+++ src/components/Dashboard/Channels/ChannelDetails/Spaces/FileDir.tsx
@@ -399,9 +399,18 @@ const FileDir: React.FC<FileDirProps> = ({ addItemToPath, findItemByPath }) => {
               <div className="grid gap-4">
                 <Input
                   placeholder="Folder name"
+                  maxLength={100}
                   onChange={(e) => {
-                    setNewFolderError("")
-                    newFolderName.current = e.target.value
+                    const value = e.target.value
+                    newFolderName.current = value
+
+                    if (value.length === 100) {
+                      setNewFolderError(
+                        "Folder name reached 100 characters limit"
+                      )
+                    } else {
+                      setNewFolderError("")
+                    }
                   }}
                 />
               </div>
