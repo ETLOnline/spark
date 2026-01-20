c39eb7b6aca44acdbb56298d2c50ac00e1813e8b
hanzla-khalid1 <hunzlakhalid12@gmail.com>
2026-01-20 16:04:11 +0500

    SW-166: task count UI design

--- src/components/Dashboard/ProjectManagement/SprintManagement/SprintBoard/BoardColumn.tsx
+++ src/components/Dashboard/ProjectManagement/SprintManagement/SprintBoard/BoardColumn.tsx
@@ -3,6 +3,7 @@ import { InsertTaskStatus, SelectSprint, SelectTask } from "@/src/db/schema"
 import BoardTaskCard from "./BoardTaskCard"
 import { useDroppable } from "@dnd-kit/core"
 import { TaskType } from "../../constants/projectManagment"
+import { Badge } from "@/src/components/ui/badge"
 
 interface Props {
   sprint?: SelectSprint
@@ -33,11 +34,13 @@ function BoardColumn({ sprint, status, tasks, onTaskClick, setTasks }: Props) {
       ref={setNodeRef}
       className="w-[33%] bg-muted/50 p-2 pb-4 rounded-xl flex-shrink-0 space-y-2"
     >
-      <div className="flex flex-row justify-between mb-4">
-        <div className="font-medium text-sm text-center">{status?.name}</div>
-        <div className="text-xs text-center ">
-          {taskCount} {taskCount === 1 ? "task" : "tasks"}
-        </div>
+      <div className="flex flex-row gap-2 items-center p-2">
+        <div className="font-medium text- text-center">{status?.name}</div>
+        {taskCount === 0 ? null : (
+          <div className="text-xs text-center bg-primary rounded  px-2 py-0.5 text-black">
+            {taskCount}
+          </div>
+        )}
       </div>
       {filteredTasks.map((task) => (
         <BoardTaskCard
