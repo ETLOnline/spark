43c3a3f6eef2eed1db78b5280b81926c9ae951da
Usama <virkusama3@gmail.com>
2026-03-27 22:39:21 +0800

    correct the param

--- src/app/api/app/rewards/add/route.ts
+++ src/app/api/app/rewards/add/route.ts
@@ -4,16 +4,16 @@ import { AddRewardAction } from "@/src/server-actions/Reward/Reward"
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json()
-    const { action_type, user_id } = body
+    const { action_type, user_id, proof_url } = body
 
-    if (!action_type || !user_id) {
+    if (!action_type || !user_id || proof_url) {
       return NextResponse.json(
-        { success: false, error: "action_type and user_id are required" },
+        { success: false, error: "action_type, proof_url and user_id are required" },
         { status: 400 }
       )
     }
 
-    const result = await AddRewardAction(action_type, user_id)
+    const result = await AddRewardAction(action_type, user_id,proof_url)
     return NextResponse.json(result, { status: 200 })
   } catch (error: any) {
     return NextResponse.json(
