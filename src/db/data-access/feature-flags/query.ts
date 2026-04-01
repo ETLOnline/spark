eea1984e0c2d54e28c9bb83e27bd12e25f5d65d8
Mazhar Manzoor <mazharkamboh914@gmail.com>
2026-04-01 20:25:46 +0500

    create query and server action to inset and update feature flags

--- src/db/data-access/feature-flags/query.ts
+++ src/db/data-access/feature-flags/query.ts
@@ -1,6 +1,10 @@
 import { eq } from "drizzle-orm"
 import { db } from "../.."
-import { featureFlagsTable, SelectFeatureFlag } from "../../schema"
+import {
+  featureFlagsTable,
+  InsertFeatureFlag,
+  SelectFeatureFlag
+} from "../../schema"
 
 export const getFeatureFlag = async (
   key: string
@@ -10,6 +14,39 @@ export const getFeatureFlag = async (
       where: eq(featureFlagsTable.key, key)
     })
     return flag
+  } catch (error: any) {
+    throw new Error(error.message)
+  }
+}
+
+export const createFeatureFlag = async (
+  data: InsertFeatureFlag
+): Promise<SelectFeatureFlag> => {
+  try {
+    const [flag] = await db.insert(featureFlagsTable).values(data).returning()
+    return flag
+  } catch (error: any) {
+    throw new Error(error.message)
+  }
+}
+
+export const updateFeatureFlag = async (
+  key: string,
+  changes: Partial<InsertFeatureFlag>
+): Promise<SelectFeatureFlag | undefined> => {
+  try {
+    const flag = await db.query.featureFlagsTable.findFirst({
+      where: eq(featureFlagsTable.key, key)
+    })
+    if (!flag) {
+      throw new Error("Feature flag not found")
+    }
+    const [updated] = await db
+      .update(featureFlagsTable)
+      .set(changes)
+      .where(eq(featureFlagsTable.key, key))
+      .returning()
+    return updated
   } catch (error: any) {
     throw new Error(error.message)
   }
