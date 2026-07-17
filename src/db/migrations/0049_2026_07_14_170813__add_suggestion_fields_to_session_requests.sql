6fbb3df9ca2adf5f5c66c1dace2d15307a045ae2
Mazhar Manzoor <mazharkamboh914@gmail.com>
2026-07-17 15:07:52 +0500

    remove migraion from journal.jsona and relative files

--- src/db/migrations/0049_2026_07_14_170813__add_suggestion_fields_to_session_requests.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "session_requests" ADD COLUMN "suggestion_message" text;--> statement-breakpoint
-ALTER TABLE "session_requests" ADD COLUMN "suggested_slot_ids" jsonb;--> statement-breakpoint
-ALTER TABLE "session_requests" ADD COLUMN "suggestion_expires_at" varchar;
