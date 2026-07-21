2c59287ce45fcb4e09a8da9810582b943115c9f4
Usamavirk444 <virkusama3@gmail.com>
2026-07-21 19:35:36 +0500

    removing migration

--- src/db/migrations/0050_2026_07_17_124904__add_repeat_fields_to_session_requests.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "session_requests" ADD COLUMN "repeat_type" varchar DEFAULT 'none' NOT NULL;--> statement-breakpoint
-ALTER TABLE "session_requests" ADD COLUMN "repeat_end_date" varchar;
--- src/db/migrations/0051_2026_07_17_203836__add_space_id_column_session_request.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE "session_requests" ADD COLUMN "space_id" varchar(36);--> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "session_requests" ADD CONSTRAINT "session_requests_space_id_spaces_id_fk" FOREIGN KEY ("space_id") REFERENCES "public"."spaces"("id") ON DELETE no action ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
