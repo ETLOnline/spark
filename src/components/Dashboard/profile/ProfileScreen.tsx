8d0249e549d863a4aaaabec144f353d4a01a8c90
Hanzla Khalid <hunzlakhalid@12gmail.com>
2026-07-24 14:48:50 +0500

    remove stars from profile header

--- src/components/Dashboard/profile/ProfileScreen.tsx
+++ src/components/Dashboard/profile/ProfileScreen.tsx
@@ -409,9 +409,6 @@ export default function ProfileScreen({
               handleCopyUrl={handleCopyUrl}
               onFileChange={handleFileChange}
               isMyProfile={isMyProfile}
-              sessionRating={
-                isMentor ? (user.profile?.total_average_rating ?? null) : null
-              }
             />
 
             {/* Trust Engine Section  */}
